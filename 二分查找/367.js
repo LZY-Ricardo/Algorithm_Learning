a5231c411ab42ef815fc37c54256e027e4f48923
RicardoLzy <3239468786lzy@gmail.com>
2025-06-09 23:51:33 +0800

    二分查找补充

---
+++ 二分查找/367.js
@@ -0,0 +1,15 @@
+var isPerfectSquare = function(num) {
+    let left = 0
+    let right = num
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (mid * mid === num) {
+            return true
+        } else if (mid * mid < num) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
+};
---
+++ 二分查找/69.js
@@ -0,0 +1,16 @@
+var mySqrt = function(x) {
+    let left = 0
+    let right = x
+    let ans = 0
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (mid * mid <= x) {
+            ans = mid 
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return ans
+};
+console.log(mySqrt(4));
